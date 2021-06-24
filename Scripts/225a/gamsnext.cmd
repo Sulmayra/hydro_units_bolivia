ca81d74f4c81d520f955a6330bdfa785acb0df25
Sulmayra <Sulmayra.zarate.guzman@gmail.com>
2021-06-24 16:07:44 +0200

    Information about Hydro Unit is added to the Dispa-SET_Bolivia2020 database

---
+++ Scripts/225a/gamsnext.cmd
@@ -0,0 +1,2 @@
+@echo off
+gmsgennt.cmd "C:\Users\VLIR_\Documents\git\hydro_units_bolivia\Scripts\225a\" "C:\Users\VLIR_\Documents\git\hydro_units_bolivia\Simulations\bolivia_base_2020\" %1 "C:\Users\VLIR_\Documents\git\hydro_units_bolivia\Scripts\225a\gamscntr.dat" "C:\GAMS\win64\24.5\" "CPLEX"
