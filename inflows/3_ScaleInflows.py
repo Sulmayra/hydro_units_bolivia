d68ae0b1ea445a30d7ff14181c71f030d6cab6a0
Sulmayra <Sulmayra.zarate.guzman@gmail.com>
2021-06-26 23:43:25 +0200

    Codes update

--- inflows/3_ScaleInflows.py
+++ inflows/3_ScaleInflows.py
@@ -7,8 +7,8 @@ Created on Tue May 11 23:03:39 2021
 
 import pandas as pd
 
-df1 = pd.read_csv("Inflows_OR.csv", index_col=0)
 
+df1 = pd.read_csv("Inflows_OR.csv", index_col=0)
 
 df2 = pd.read_csv("MainData.csv", index_col=0)
 # To Convert DataFrame to Series
@@ -21,10 +21,10 @@ df4 = df3.dropna(1)
 
 # Converting the index as data
 df4.index = pd.to_datetime(df4.index)
-#df4.index = pd.to_datetime(df4.index,freq='H')
 
-
-df4.to_csv("ScaledInflows_OR.csv")
-
-
-
+new_index = pd.date_range(start="1980-01-01 00:00", end="2015-12-31 23:55",freq="1h")
+df4 = df4.reindex(index=new_index)
+df5= df4.fillna(method="pad")
+df5.index.names = ["TIMESTAMP"]
+df5.to_csv("ScaledInflows_OR.csv")
+print(df5.describe())
--- inflows/4_Inflow_by_year.py
+++ inflows/4_Inflow_by_year.py
@@ -7,23 +7,26 @@ Created on Sat May 15 07:12:57 2021
 
 import pandas as pd
 
-data = pd.read_csv("Inflows_NO.csv", index_col=0)
-
+data = pd.read_csv("ScaledInflows_CE.csv")
+data.TIMESTAMP = pd.to_datetime(data.TIMESTAMP)
+data = data.squeeze()
 
 for i in range(1980,2016):
     startdate = "01-01-"+str(i)+" 00:00" 
     endate = "12-31-"+str(i)+" 23:55"  
     idx = pd.date_range(start=startdate, end=endate,freq="1h") 
-    df = pd.DataFrame(range(len(idx)), index=idx)
-    df = data.copy()
-    filename="./export_dataframe/" + str(i) + ".csv"
+    df = pd.DataFrame(idx)
+    df.columns= ["TIMESTAMP"]
+    df1 = df.set_index("TIMESTAMP").join(data.set_index("TIMESTAMP"))
+    filename="../HydroData/ScaledInflows/CE/" + str(i) + ".csv"
     print(filename)
-    df.to_csv(filename)
+    df1.to_csv(filename)
     
 
     
 
 
+    
     
 
 
