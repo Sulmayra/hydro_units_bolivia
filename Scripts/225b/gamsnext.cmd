15d3c16cdce02429d108eb44f13ce6f361600599
Sulmayra <Sulmayra.zarate.guzman@gmail.com>
2022-03-14 23:26:27 +0100

    .

--- Scripts/225b/gamsnext.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-gmsgennt.cmd "C:\Users\VLIR_\Documents\git\hydro_units_bolivia\Scripts\225b\" "C:\Users\VLIR_\Documents\git\hydro_units_bolivia\Simulations\bolivia_base_2020_SIN\" %1 "C:\Users\VLIR_\Documents\git\hydro_units_bolivia\Scripts\225b\gamscntr.dat" "C:\GAMS\win64\24.5\" "CPLEX"
