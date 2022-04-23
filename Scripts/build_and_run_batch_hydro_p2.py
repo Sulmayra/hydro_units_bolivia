633e460aa2302d90c06d3bdfb04cd43311cc1c7e
Sulmayra <Sulmayra.zarate.guzman@gmail.com>
2022-04-23 13:35:08 +0200

    Read results.

--- Scripts/build_and_run_batch_hydro_p2.py
+++ Scripts/build_and_run_batch_hydro_p2.py
@@ -16,16 +16,15 @@ import dispaset as ds
 # Load the configuration file
 config = ds.load_config_excel('../ConfigFiles/ConfigBO_2020_HU.xlsx')
 
-data = pd.DataFrame(index = range(1980,1982))
-for i in range(1980,1982):
+data = pd.DataFrame(index = range(1980,2016))
+for i in range(1980,2016):
     config['SimulationDirectory']  = config['SimulationDirectory']+ '/' + str(i)
     sim_folder = config['SimulationDirectory'] 
-    # sim_folder = config['SimulationDirectory'] + '/' + str(i)
 
     # Load inputs and results to memory
     inputs, results = ds.get_sim_results(path=sim_folder, cache=True)
     
-    
+   
     # Save inputs and results to a file
     # Analyse the results for each country and provide quantitative indicators:
     r = ds.get_result_analysis(inputs,results)
